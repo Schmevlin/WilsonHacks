e7c972e0fc00662d296f7acecd5118d48c74f7d0
VirtualMe64 <43835028+VirtualMe64@users.noreply.github.com>
2020-02-01 12:04:32 -0500

    movement

--- Main.py
+++ Main.py
@@ -16,15 +16,11 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:    
-            if event.key == pygame.key.K_w:
-                player.move(1, 0)
-            if event.key == K_S:
-                player.move(-1, 0)
-            if event.key == K_D:
-                player.move(0, 1)
-            if event.key == K_A:
-                player.move(0, -1)
+        
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_w]:
+        player.move(0, 1)
 
     screen.fill(white)
 
--- Player.py
+++ Player.py
@@ -1,15 +1,15 @@
 import pygame
 
 class player(pygame.sprite.Sprite):
-
     def __init__(self, width, height, x, y, screen):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Rect(x, y, width, height)
         self.screen = screen
         self.x, self.y = x, y
+        self.width, self.height = width, height
     
     def draw(self):
-        pygame.draw.rect(self.screen, (0, 0, 0), self.image)
+        pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(self.x, self.y, self.width, self.height))
 
     def move(self, x, y):
         self.x += x
